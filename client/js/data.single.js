e7bac646bc1ee6f7428c47b61201c994d76df572
Ben Galbraith <ben@galbraiths.org>
2010-01-05 04:30:33 -0800

    Fixes related to getting updated data into the system

--- client/js/data.single.js
+++ client/js/data.single.js
@@ -12,102 +12,43 @@ var data = {
     "countryCode": "US",
     "items": [
       {
-        "title": "Mine Search",
-        "link": "http://developer.palm.com/appredirect/?packageid=com.engineequalscar.games.mines",
-        "description": "Mine searcher is our take on this classic game.",
-        "pubDate": "2009-01-21 21:00:58.0 UTC",
-        "guid": "6.6",
-        "packageid": "com.engineequalscar.games.mines",
-        "version": "0.9.5",
-        "installed_size": "1038810",
-        "rating": "2.9",
-        "total_downloads": "1322",
-        "total_comments": "17",
-        "minOs": "webOS 1.0.0",
-        "devices": "Palm Pre, Palm Pixi",
-        "language": "en",
-        "country": "US",
-        "icons": [
-          {
-            "type": "app",
-            "url": "http://cdn.downloads.palm.com/public/6/icon/icon_252D5E.png"
-          },
-          {
-            "type": "scaled",
-            "url": "http://cdn.downloads.palm.com/public/6/icon/S/icon_252D5E.png"
-          },
-          {
-            "type": "large",
-            "url": "http://cdn.downloads.palm.com/public/6/icon/L/icon_big.png"
-          }
-        ],
-        "localizations": [
-          {
-            "country": "US",
-            "language": "en",
-            "title": "Mine Search",
-            "summary": "Mine searcher is our take on this classic game. The object of the game is to select tiles to clear the field and to avoid mines. As the tiles are selected the number of mines that are adjacent to that tile is revealed to help the player determine the mines' locations. The game allows you to change board size and the number of mines in the field for varying difficulty and endless fun. Records are kept so you can keep track of your best times.-- If you had the beta version you must uninstall that prior to downloading.",
-            "price": {
-              "currency": "USD",
-              "amount": 0.99000000953674
-            },
-            "developer": "Engine Equals Car",
-            "developer_url": "http://www.engineequalscar.com",
-            "supportUrl": "http://www.engineequalscar.com",
-            "categories": {
-              "content": [
-                "Games!Arcade",
-                "Games!Classics",
-                "Games!Strategy"
-              ]
-            },
-            "images": {
-              "image_1": [
-                {
-                  "type": "app",
-                  "url": "http://cdn.downloads.palm.com/public/6/en/images/1/A/mines_2009-08-09_061656.jpg"
-                },
-                {
-                  "type": "scaled",
-                  "url": "http://cdn.downloads.palm.com/public/6/en/images/1/S/mines_2009-08-09_061656.jpg"
-                },
-                {
-                  "type": "large",
-                  "url": "http://cdn.downloads.palm.com/public/6/en/images/1/L/mines_2009-08-09_061656.jpg"
-                }
-              ],
-              "image_2": [
-                {
-                  "type": "app",
-                  "url": "http://cdn.downloads.palm.com/public/6/en/images/2/A/mines_2009-20-08_071859_256A3E.jpg"
-                },
-                {
-                  "type": "scaled",
-                  "url": "http://cdn.downloads.palm.com/public/6/en/images/2/S/mines_2009-20-08_071859_256A3E.jpg"
-                },
-                {
-                  "type": "large",
-                  "url": "http://cdn.downloads.palm.com/public/6/en/images/2/L/mines_2009-20-08_071859_256A3E.jpg"
-                }
-              ],
-              "image_3": [
-                {
-                  "type": "app",
-                  "url": "http://cdn.downloads.palm.com/public/6/en/images/3/A/mines_2009-08-09_061855.jpg"
-                },
-                {
-                  "type": "scaled",
-                  "url": "http://cdn.downloads.palm.com/public/6/en/images/3/S/mines_2009-08-09_061855.jpg"
-                },
-                {
-                  "type": "large",
-                  "url": "http://cdn.downloads.palm.com/public/6/en/images/3/L/mines_2009-08-09_061855.jpg"
-                }
-              ]
-            }
-          }
-        ]
-      }
+"title": "Mine Search",
+"description": "Mine searcher is our take on this classic game. The object of the game is to select tiles to clear the \"field\" and to avoid mines. As the tiles are selected the number of mines that are adjacent to that tile is revealed to help the player determine the mines' locations. The game allows you to change board size and the number of mines in the field for varying difficulty and endless fun. Records are kept so you can keep track of your best times.\u000a\u000a-- If you had the beta version you must uninstall that prior to downloading.",
+"pubDate": "2009-12-21 21:00:58",
+"guid": "6.6",
+"packageid": "com.engineequalscar.games.mines",
+"version": "0.9.5",
+"installed_size": "1038810",
+"rating": "2.9",
+"total_downloads": "1352",
+"total_comments": "17",
+"total_accounts": "1270",
+"min_os": "webOS 1.0.0",
+"devices": "Palm Pre, Palm Pixi",
+"language": "en",
+"country": "US",
+"icons": [{
+"type": "app",
+"url": "http://cdn.downloads.palm.com/public/121/icon/icon_252D5E.png"
+}
+, {
+"type": "scaled",
+    "url": "http://cdn.downloads.palm.com/public/121/icon/S/icon_252D5E.png"
+}
+],
+"localizations": [{
+"country": "US",
+"language": "en",
+"title": "Mine Search",
+"summary": "Mine searcher is our take on this classic game. The object of the game is to select tiles to clear the field and to avoid mines. As the tiles are selected the number of mines that are adjacent to that tile is revealed to help the player determine the mines' locations. The game allows you to change board size and the number of mines in the field for varying difficulty and endless fun. Records are kept so you can keep track of your best times.\u000a\u000a-- If you had the beta version you must uninstall that prior to downloading.",
+"price": "0.99",
+"developer": "Engine Equals Car",
+"developer_url": "http://www.engineequalscar.com",
+"support_url": "http://www.engineequalscar.com",
+"categories": ["Games", "Games!Arcade", "Games!Classics", "Games!Strategy"],
+"images": ["http://cdn.downloads.palm.com/public/121/en/images/1/L/mines_2009-08-09_061656.jpg", "http://cdn.downloads.palm.com/public/121/en/images/2/L/mines_2009-20-08_071859_256A3E.jpg", "http://cdn.downloads.palm.com/public/121/en/images/3/L/mines_2009-08-09_061855.jpg"],
+
+}]}
     ]
   }
 };
